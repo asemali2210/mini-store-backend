dfe4cd2cbde66ffd54e234dc01460d4adf226760
asemali <asemali2210@gmail.com>
2025-07-10 11:58:46 +0300

    first init

---
+++ src/api/hero-home/controllers/hero-home.ts
@@ -0,0 +1,7 @@
+/**
+ * hero-home controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::hero-home.hero-home');
---
+++ src/api/hero-home/routes/hero-home.ts
@@ -0,0 +1,7 @@
+/**
+ * hero-home router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::hero-home.hero-home');
